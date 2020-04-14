6078c996fe8d4314c057101616b649497c9795af
Diana Gornic <gornicdiana.199@gmail.com>
2020-04-14 14:59:15 +0300

    SS-67/created the stock database

--- src/proiect/fis/store/model/databases/StockDB.java
+++ src/proiect/fis/store/model/databases/StockDB.java
@@ -5,42 +5,42 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DemandsDB {
+public class StockDB {
+    public static final String TABLE_NAME = "stock";
     public static final String DB_NAME = "store.db";
     private static String filepath = System.getProperty("user.home") + System.getProperty("file.separator") + DB_NAME;
     public static final String CONNECTION_STRING = "jdbc:sqlite:" + filepath;
-    public static final String TABLE_NAME = "Demands";
+
     private Connection connection;
-
-    public static DemandsDB instance = new DemandsDB();
-
-    private DemandsDB() {
-
-    }
-
-    public static DemandsDB getInstance() {
+    private static StockDB instance = new StockDB();
+    private StockDB() {}
+    public static StockDB getInstance() {
         return instance;
     }
 
     public boolean openConnection() {
-        try {
+
+        try{
             connection = DriverManager.getConnection(CONNECTION_STRING);
             Statement statement = connection.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS " + DB_NAME + " (name TEXT,price REAL,quantity INTEGER);");
             return true;
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             System.out.println("create table error");
             return false;
         }
+
     }
 
-    public void closeConnection() {
+    public void closeConnection () {
         try {
-            if(connection != null)
+            if (connection!=null)
                 connection.close();
-
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             //
         }
     }
+
 }
