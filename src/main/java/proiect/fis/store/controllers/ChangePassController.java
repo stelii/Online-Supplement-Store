82853196fdd1755ae150f579dade99f82f54f349
Stelian <fedorca.stelian@home.com>
2020-04-28 00:52:38 +0300

    Created change password page

---
+++ src/main/java/proiect/fis/store/controllers/ChangePassController.java
@@ -0,0 +1,67 @@
+package proiect.fis.store.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import proiect.fis.store.model.Customer;
+import proiect.fis.store.model.DataSource;
+import proiect.fis.store.model.EncryptPassword;
+
+import java.io.IOException;
+
+public class ChangePassController {
+    @FXML
+    private Button backToMainPage;
+    @FXML
+    private Button changePassBtn;
+    @FXML
+    private PasswordField passwordInput;
+
+    private Customer customer ;
+
+    public void initialize(){
+//        System.out.println("From initialize method: " + customer.getName());
+        backToMainPage.setDisable(true);
+    }
+
+    public void initData(Customer customer){
+        this.customer = customer ;
+    }
+
+
+    @FXML
+    public void switchToMainPage() {
+        try {
+            Stage stage = (Stage) backToMainPage.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/customer_page.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void changePassword(){
+        DataSource dataSource = DataSource.getInstance();
+        String username = customer.getUsername();
+        String password = passwordInput.getText();
+        System.out.println("new password: " + password);
+
+        passwordInput.clear();
+
+        String hashedPassword = EncryptPassword.encrypt(password);
+        if(dataSource.updatePassword(username,hashedPassword)){
+            backToMainPage.setDisable(false);
+            return ;
+        }
+
+        System.out.println("i didn't succeed");
+    }
+
+}
