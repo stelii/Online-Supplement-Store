7ff11fd1353a57999cdc8c79518f660a408ebc8b
Diana Gornic <gornicdiana.199@gmail.com>
2020-04-30 15:12:45 +0300

    Update list of available products

--- src/main/java/proiect/fis/store/model/databases/StockDB.java
+++ src/main/java/proiect/fis/store/model/databases/StockDB.java
@@ -12,6 +12,8 @@ public class StockDB {
     private static String filepath = System.getProperty("user.home") + System.getProperty("file.separator") + DB_NAME;
     public static final String CONNECTION_STRING = "jdbc:sqlite:" + filepath;
 
+    public static final String WITHDRAW_QUANTITY = "UPDATE " + TABLE_NAME + " SET quantity = quantity - ? WHERE name = ?";
+
     private Connection connection;
     private static StockDB instance = new StockDB();
     private StockDB() {}
@@ -50,6 +52,20 @@ public class StockDB {
         }catch (SQLException e){
             //
             return null ;
+        }
+    }
+
+    public boolean withdrawQuantity(Product product,int quantity){
+        String name = product.getName();
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(WITHDRAW_QUANTITY)){
+            preparedStatement.setInt(1,quantity);
+            preparedStatement.setString(2,name);
+            int rez = preparedStatement.executeUpdate();
+            return rez > 0 ;
+        }catch (SQLException e){
+            //
+            return false;
         }
     }
 
